5679115e8e47d2bab393b90a8babfb36514efecb
MiMac <miigon.pg@gmail.com>
2020-10-11 11:01:40 +0800

    fix backend bugs, newUser finished

--- cloud/functions/getUserDetails/index.js
+++ cloud/functions/getUserDetails/index.js
@@ -5,11 +5,30 @@ cloud.init({env: cloud.DYNAMIC_CURRENT_ENV})
 const db = cloud.database()
 
 exports.main = async (event, context) => {
-  const { OPENID } = cloud.getWXContext()
-  return await db.collection('users').where({
+  let OPENID = event.userInfo.openId;
+  return db.collection('users').where({
     openId: OPENID
   })
   .limit(1)
   .get()
+  .then(result => {
+    if(result.data.length == 0) {
+      // 创建用户在数据库中的记录
+      let newUserData = {isVip: false, phone: ""}
+      console.log("newUser:",newUserData,"openid:",OPENID);
+      return cloud.callFunction({
+        name: "changeUserDetails",
+        data: {userInfo: {openId: OPENID}, userData: newUserData},
+      })
+      .then(()=>{
+        return newUserData;
+      })
+    } else {
+      return new Promise(resolve=>{resolve(result.data[0])});
+    }
+  })
+  .then(data => {
+    return data;
+  })
 }
 
