011379551b2fbb0e280b3ad83329384d7523188d
MiMac <miigon.pg@gmail.com>
2020-10-19 23:19:05 +0800

    实习证明: 新样式,修复背景透明,增强兼容性

--- client/src/pages/mine/certificate/index.js
+++ client/src/pages/mine/certificate/index.js
@@ -1,9 +1,14 @@
 let nameInput="";
 let ctx=null;
-const img1="https://7475-tutor-ghszz-1303852457.tcb.qcloud.la/images/certificate/1.png?sign=78eaad9bd77e6899f723c698d51a957c&t=1602490228";
-const img2="https://7475-tutor-ghszz-1303852457.tcb.qcloud.la/images/certificate/2.png?sign=07134ef00af16e67f787653d950c28bc&t=1602490291";
-const img3="https://7475-tutor-ghszz-1303852457.tcb.qcloud.la/images/certificate/3.png?sign=b9e6f413656fa9f650a75d100ebb3bf0&t=1602490306";
-const img4="https://7475-tutor-ghszz-1303852457.tcb.qcloud.la/images/certificate/4.png?sign=0e2a02338f7b7ff8a9023cb1e780c376&t=1602490314";
+
+const imgPrefix="https://7475-tutor-ghszz-1303852457.tcb.qcloud.la/images/certificate/";
+const allImgs = [
+  { x: 0, y: 0, w: 640, h: 120, src: "0.png"},
+  { x: 57, y: 129, w: 526, h: 50, src: "1.png"},
+  { x: 57, y: 200, w: 526, h: 60, src: "2.png"},
+  { x: 57, y: 395, w: 526, h: 300, src: "3.png"},
+  { x: 57, y: 695, w: 526, h: 158, src: "4.png"},
+]
 
 Page({
   onLoad: function (options) {},
@@ -26,69 +31,47 @@ Page({
   init(res) {
     const canvas = res[0].node;
     ctx = canvas.getContext("2d");
-
-    const dpr = wx.getSystemInfoSync().pixelRatio;
-
-    const width = res[0].width;
-    const height = res[0].height;
-
-
-    canvas.width = width * dpr;
-    canvas.height = height * dpr;
-    ctx.scale(dpr, dpr);
     
-   this.drawImg1(canvas,ctx,width);
-   this.drawImg2(canvas,ctx,width);
-   this.drawText(ctx);
-   this.drawImg3(canvas,ctx,width);
-   this.drawImg4(canvas,ctx,width);
-   
+    canvas.width = 640;
+    canvas.height = 940;
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+
+    this.drawImgGroup(canvas,ctx,allImgs);
+    this.drawText(ctx);
+    ctx.font = "36px sans-serif";
   },
 
-  drawImg1(canvas,ctx,width){
-    const headerImg = canvas.createImage();
-    headerImg.src = img1; //
-    headerImg.onload = () => {
-      // 图片的x坐标=canvas的宽度的一半-图像的一半
-      ctx.drawImage(headerImg, 30,20,width-60,30);
-    };
+  drawImgGroup(canvas,ctx,imgs){
+    for(img of imgs) {
+      const image = canvas.createImage();
+      image.src = imgPrefix + img.src;
+      
+      let i=img;
+      image.onload = () => {
+        ctx.drawImage(image,i.x,i.y,i.w,i.h);
+      };
+    }
   },
-  drawImg2(canvas,ctx,width){
-    const img = canvas.createImage();
-    img.src = img2; 
-    img.onload = () => {
-      // 图片的x坐标=canvas的宽度的一半-图像的一半
-      ctx.drawImage(img, 30,55,width-60,30);
-    };
-  }, 
+
   drawText(ctx){
-    ctx.fillText("兹证明",30,105);
-    ctx.beginPath();
-    ctx.moveTo(70,105);
-    ctx.lineTo(130,105);
-    ctx.stroke();
-   
- },
-  drawImg3(canvas,ctx,width){
-    const img = canvas.createImage();
-    img.src = img3; 
-    img.onload = () => {
-      // 图片的x坐标=canvas的宽度的一半-图像的一半
-      ctx.drawImage(img, 30,120,width-60,150);
-    };
-  }, 
-  drawImg4(canvas,ctx,width){
-    const img = canvas.createImage();
-    img.src = img4; 
-    img.onload = () => {
-      // 图片的x坐标=canvas的宽度的一半-图像的一半
-      ctx.drawImage(img, 30,270,width-60,79);
-    };
-  },
+    ctx.fillStyle = "#888888";
+    ctx.strokeStyle = "#888888";
+    ctx.font = "24px sans-serif";
 
-  drawName(){
-    ctx.clearRect(70,95,100,10);
-    ctx.fillText(`${nameInput}`,70,104);
+    ctx.fillText("兹证明",57,328);
+    ctx.beginPath();
+    ctx.moveTo(145,337);
+    ctx.lineTo(275,337);
+    ctx.stroke();
+ },
+
+  drawName(nameInput){
+    ctx.fillStyle = "white";
+    ctx.fillRect(145,297,400,38);
+    ctx.fillStyle = "black";
+    ctx.fillText(nameInput,145,328);
   },
 
   async saveImg() {
